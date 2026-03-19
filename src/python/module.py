ffdb8ca767507657fd018e2858534a5c7cbc933b
Ivan Velickovic <i.velickovic@unsw.edu.au>
2026-03-19 11:56:12 +1100

    python: fix physical MR creation for paddr of address 0
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- src/python/module.py
+++ src/python/module.py
@@ -644,7 +644,7 @@ class SystemDescription:
             paddr: Optional[int] = None
         ) -> None:
             c_name = c_char_p(name.encode("utf-8"))
-            if paddr:
+            if paddr is not None:
                 physical = True
             if physical:
                 self._obj = libsdfgen.sdfgen_mr_create_physical(sdf._obj, c_name, size, ffi_uint64_ptr(paddr))
